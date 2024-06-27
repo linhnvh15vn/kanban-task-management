9ffe3bf9ed83a109a327ec5f605d7778f88bdbf2
linhnvh15vn <linhnvh15vn@gmail.com>
2024-06-27 22:41:14 +0700

    refactor: use zustand for toggle nav in main layout

--- src/app/boards/[boardId]/page.tsx
+++ src/app/boards/[boardId]/page.tsx
@@ -3,31 +3,23 @@ import React from "react";
 import { redirect } from "next/navigation";
 
 import KanbanBoard from "~/components/kanban-board";
-import Navigation from "~/components/navigation";
-import NavigationOpenSidebar from "~/components/navigation/navigation-open-sidebar";
 import { api } from "~/trpc/server";
 
 interface Props {
   params: {
     boardId: number;
   };
-  searchParams: {
-    nav?: boolean;
-  };
 }
 
-export default async function Page({ params, searchParams }: Props) {
+export default async function Page({ params }: Props) {
   const board = await api.board.getById({ id: Number(params.boardId) });
   if (!board) {
     return redirect("/");
   }
 
   return (
-    <div className="grid flex-1 md:grid-cols-[260px_minmax(0,1fr)] xl:grid-cols-[300px_minmax(0,1fr)]">
-      {searchParams.nav && <Navigation />}
-      {!searchParams.nav && <NavigationOpenSidebar />}
-
+    <main className="overflow-x-auto">
       <KanbanBoard board={board} />
-    </div>
+    </main>
   );
 }
--- src/store/use-global-store.ts
+++ src/store/use-global-store.ts
@@ -1,1 +1,11 @@
 import { create } from "zustand";
+
+interface GlobalStore {
+  isNav: boolean;
+  toggleNav: () => void;
+}
+
+export const useGlobalStore = create<GlobalStore>((set) => ({
+  isNav: true,
+  toggleNav: () => set((state) => ({ isNav: !state.isNav })),
+}));
