e16ab7c66efdaeb00724403d7363f63a5f31d8bb
linhnvh15vn <linhnvh15vn@gmail.com>
2024-07-16 16:34:41 +0700

    refactor: remove toStrin()

--- src/components/navigation/navigation-item.tsx
+++ src/components/navigation/navigation-item.tsx
@@ -21,7 +21,7 @@ export default function NavigationItem({ board }: Props) {
       href={`/boards/${board.id}`}
       className={cn(
         "flex h-12 items-center gap-3 rounded-e-full px-6 text-muted-foreground transition-colors",
-        params.boardId === board.id.toString()
+        params.boardId === board.id
           ? "bg-primary text-primary-foreground hover:bg-primary-hover"
           : "hover:bg-secondary hover:text-secondary-foreground",
       )}
