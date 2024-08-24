91260a6da53420c587a8d24e75fe32e83441fb83
linhnvh15vn <linhnvh15vn@gmail.com>
2024-08-24 22:32:24 +0700

    refactor: simplify BOARD_FORM modal trigger

--- src/components/navigation/navigation-create-button.tsx
+++ src/components/navigation/navigation-create-button.tsx
@@ -12,7 +12,7 @@ export default function NavigationCreateButton() {
   return (
     <div
       className="flex h-12 cursor-pointer items-center gap-3 rounded-e-full px-6 text-primary hover:bg-secondary hover:text-primary"
-      onClick={() => onOpen("BOARD_FORM", { task: null })}
+      onClick={() => onOpen("BOARD_FORM")}
     >
       <SquareKanban />
       <h3>+ Create New Board</h3>
